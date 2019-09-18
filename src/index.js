7b38d1aa85c18c0e7695e8969f6bdd2207e776c7
Jose Dominic <josudominic007@gmail.com>
2019-09-18 21:03:03 +0530

    finished backend,working on react components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'; //importing react app from App.js file
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root')); //renders frontend in the html(index.html) element with id root
+                                                            
+
+
