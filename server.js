37c86917a1a3d8db28e84a0ff7d640df558c9a05
josedominic <josudominic007@gmail.com>
2019-09-22 14:33:47 +0530

    v1.1

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express(); //creating the app
 //Serve static assets if in production
 if(process.env.NODE_ENV ==='production'){
 	//Set static folder
-	app.use(express.static('../build'));
+	app.use(express.static('client/build'));
 
 	app.get('*',(req, res) => {
-		res.sendFile(path.resolve(__dirname,'/../','build','index.html'));
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 	});
 }
 ////////////////////////////////////////
